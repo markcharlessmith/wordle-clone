89affa56d8e68865a20b5ae1286c8a46a7b9b429
Mark Charles Smith <markcharlessmith@gmail.com>
2022-06-22 15:55:36 -0700

    added App.jsx code

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import "./styles.css";
+import { useEffect, useState } from "react";
+import wordleWords from "./wordleWords.js";
+
+const WORD_LENGTH = 5;
+
+export default function App() {
+  const [solution, setSolution] = useState("");
+  const [guesses, setGuesses] = useState(Array(6).fill(null));
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [isGameOver, setIsGameOver] = useState(false);
+
+  useEffect(() => {
+    const handleType = (event) => {
+      if (isGameOver) {
+        return;
+      }
+
+      if (event.key === "Enter") {
+        if (currentGuess.length !== 5) {
+          return;
+        }
+
+        const newGuesses = [...guesses];
+        newGuesses[guesses.findIndex((val) => val == null)] = currentGuess;
+        setGuesses(newGuesses);
+        setCurrentGuess("");
+
+        const isCorrect = solution === currentGuess;
+        if (isCorrect) {
+          setIsGameOver(true);
+        }
+      }
+
+      if (event.key === "Backspace") {
+        setCurrentGuess(currentGuess.slice(0, -1));
+        return;
+      }
+
+      if (currentGuess.length >= 5) {
+        return;
+      }
+
+      const isLetter = event.key.match(/^[a-z]{1}$/) != null;
+      if (isLetter) {
+        setCurrentGuess((oldGuess) => oldGuess + event.key);
+      }
+    };
+
+    window.addEventListener("keydown", handleType);
+    return () => window.removeEventListener("keydown", handleType);
+  }, [currentGuess, isGameOver, solution, guesses]);
+
+  useEffect(() => {
+    const getWord = async () => {
+      const randomWord = await wordleWords[
+        Math.floor(Math.random() * wordleWords.length)
+      ];
+      setSolution(randomWord.toLowerCase());
+      // console.log(solution);
+    };
+    getWord();
+  }, []);
+  return (
+    <div className="board">
+      {guesses.map((guess, i) => {
+        const isCurrentGuess = i === guesses.findIndex((val) => val == null);
+        return (
+          <Line
+            guess={isCurrentGuess ? currentGuess : guess ?? ""}
+            isFinal={!isCurrentGuess && guess != null}
+            solution={solution}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function Line({ guess, isFinal, solution }) {
+  const tiles = [];
+  for (let i = 0; i < WORD_LENGTH; i++) {
+    const char = guess[i];
+    let className = "tile";
+
+    if (isFinal) {
+      // console.log(solution);
+      if (char === solution[i]) {
+        className += " correct";
+      } else if (solution.includes(char)) {
+        className += " close";
+      } else {
+        className += " incorrect";
+      }
+    }
+
+    tiles.push(
+      <div key={i} className={className}>
+        {char}
+      </div>
+    );
+  }
+  return <div className="line">{tiles}</div>;
+}
