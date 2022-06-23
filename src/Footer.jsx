316b627d81809429994a07242c2b4bc05f350bd7
Mark Charles Smith <markcharlessmith@gmail.com>
2022-06-23 15:35:55 -0700

    added Footer.jsx component

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">practice your skills
+    <br></br>
+    <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/google/313/nerd-face_1f913.png" width="100" height="100"></img>
+    </div>
+  )
+}
