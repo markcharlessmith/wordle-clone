5aa21f1066d321643ca29f9f4faa92f2beeec658
Mark Charles Smith <markcharlessmith@gmail.com>
2022-06-23 16:23:23 -0700

    updated Footer.jsx and styles.css

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import react from "react";
 
 export default function Footer() {
   return (
-    <div className="footer">practice your skills
+    <div className="footer">PRACTICE YOUR SKILLS
     <br></br>
     <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/google/313/nerd-face_1f913.png" width="100" height="100"></img>
     </div>
